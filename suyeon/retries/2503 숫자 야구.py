bd241efa69f37670c07f2615df4d198d596b530c
Suyeon-B <tndus4243@naver.com>
2022-04-08 02:09:49 +0900

    retry

--- suyeon/retries/2503 숫자 야구.py
+++ suyeon/retries/2503 숫자 야구.py
@@ -1,5 +1,5 @@
 # 시험 때 풀지 못한 문제 retry
-# 
+# 내일 다시 retry
 
 import itertools
 import sys
@@ -8,18 +8,22 @@ n = int(sys.stdin.readline().strip())
 
 data = ['1', '2', '3', '4', '5', '6', '7', '8', '9']
 NUMBERS = list(itertools.permutations(data, 3))
-print(NUMBERS)
 
-for NUM in NUMBERS:
+remove_cnt = 0
+for _ in range(n):
     number, s, b = input().split()
-    strike = ball = 0
-    for i in range(3):
-        if NUM[i] == number[i]:
-            strike += 1
-        elif NUM[i] in number:
-            ball += 1
-    
-    if int(s) != strike or int(b) != ball:
-        NUMBERS.remove(NUM)
+    remove_cnt = 0
+    for i in range(len(NUMBERS)):
+        strike = ball = 0
+        i -= remove_cnt
+        for j in range(3):
+            if NUMBERS[i][j] == number[j]:
+                strike += 1
+            elif number[j] in NUMBERS[i]:
+                ball += 1
+        
+        if int(s) != strike or int(b) != ball:
+            NUMBERS.remove(NUMBERS[i])
+            remove_cnt += 1
 
 print(len(NUMBERS))
